99510e5391bcae4aa9e69ad7ebe9db00922385df
Qurat-UlAin9 <qura890@gmail.com>
2026-05-09 14:03:48 +0500

    added pytest dependency

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,16 +1,15 @@
 import pytest
-from app import app
+from app import app, bootstrap_database, train_or_load_model
 
 
 @pytest.fixture
 def client():
-    """
-    Creates Flask test client.
 
-    This allows us to simulate API requests
-    without running the real server.
-    """
-     app.config['TESTING'] = True
+    # Initialize DB + model before tests
+    bootstrap_database()
+    train_or_load_model()
+
+    app.config['TESTING'] = True
 
     with app.test_client() as client:
         yield client
